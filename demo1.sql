6f886cab5b262b33c778720bbc2fc05888435026
ashleygoce <94890040+sreneee@users.noreply.github.com>
2023-03-21 12:19:08 +0800

    Add files via upload

--- demo1.sql
+++ demo1.sql
@@ -1,1 +1,5 @@
-SELECT * FROM testdatabase.sample;
+SELECT * FROM testdatabase.personstable;
+CREATE TABLE orderstable(OrderID INT NOT NULL, OrderNumber INT NOT NULL, PRIMARY KEY (OrderID));
+orderstableINSERT INTO orderstable (OrderID, OrderNumber) VALUES ('1', '778895'),
+																('2', '44678'),
+																('3', '22456');
